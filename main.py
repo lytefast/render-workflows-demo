dc92bfdb536a7dd2c28aa1532bcd53401c3ba6f3
Sam S <lytefast@gmail.com>
2026-05-11 14:01:33 -0700

    subtask retries

--- main.py
+++ main.py
@@ -27,9 +27,29 @@ async def sum_squares(a: int, b: int) -> int:
     )
 )
 def flip_coin() -> str:
-    if random.random() < 0.5:
+    if random.random() < 0.95:
         raise Exception("Flipped tails! Retrying.")
     return "Flipped heads!"
+
+@app.task(
+    retry=Retry(
+        max_retries=3,
+        wait_duration_ms=3000,
+        backoff_scaling=1.5,
+    )
+)
+def flip_coin_2() -> str:
+    if random.random() < 0.55:
+        raise Exception("Flipped tails! Retrying.")
+    return "Flipped heads!"
+
+@app.task
+async def double_flip_coin() -> int:
+    result1, result2 = await asyncio.gather(
+        flip_coin_2(),
+        flip_coin_2(),
+    )
+    return result1 == result2
 
 
 if __name__ == "__main__":
